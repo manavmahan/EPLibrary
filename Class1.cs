47104972a248dcc6df0acd5089f1fe80da45c9c5
Manav Mahan Singh <manav.mahan.singh@live.in>
2020-01-06 10:19:36 +0100

    1

--- Class1.cs
+++ Class1.cs
@@ -2699,12 +2699,13 @@ namespace IDFFile
     public class People
     {
         public People() { }
+        
+        string Name, ZoneName;
         public string scheduleName { get; set; }
         public string calculationMethod { get; set; }
         //public double numberOfPeople { get; set; }
         //public double peoplePerArea { get; set; }
         public double areaPerPerson { get; set; }
-
         public double fractionRadiant { get; set; }
         public double sensibleHeatFraction { get; set; }
         public string activityLvlSchedName { get; set; }
@@ -2713,14 +2714,11 @@ namespace IDFFile
         public string meanRadiantTempCalcType { get; set; }
         public string surfaceName { get; set; }
         public string workEffSchedule { get; set; }
-
         public string clothingInsulationCalcMeth { get; set; }
         public string clothingInsulationCalcMethSched { get; set; }
         public string clothingInsulationSchedName { get; set; }
-
         public string airVelSchedName { get; set; }
         public string thermalComfModel1t { get; set; }
-
         public People(double aPP)
         {
             areaPerPerson = aPP;
@@ -2729,17 +2727,42 @@ namespace IDFFile
             fractionRadiant = 0.1;
             activityLvlSchedName = "People Activity Schedule";
             c02genRate = double.Parse("3.82E-8");
-
             enableComfortWarnings = "";
             meanRadiantTempCalcType = "ZoneAveraged";
             surfaceName = "";
-
             workEffSchedule = "Work Eff Sch";
             clothingInsulationCalcMeth = "DynamicClothingModelASHRAE55";
             clothingInsulationCalcMethSched = "";
             clothingInsulationSchedName = "";
             airVelSchedName = "Air Velo Sch";
             thermalComfModel1t = "Fanger";
+        }
+        public List<string> WriteInfo()
+        {
+            return new List<string>()
+            {
+                "People,",
+                Utility.IDFLineFormatter(Name, "Name"),
+                Utility.IDFLineFormatter(ZoneName, "Zone or ZoneList Name"),
+                Utility.IDFLineFormatter(scheduleName, "Schedule Name"),
+                Utility.IDFLineFormatter(calculationMethod, "Number of People Calculation Method"),
+                Utility.IDFLineFormatter("", "Number of People"),
+                Utility.IDFLineFormatter("","People per Zone Floor Area {person/m2}"),
+                Utility.IDFLineFormatter(areaPerPerson, "Zone Floor Area per Person {m2/person}"),
+                Utility.IDFLineFormatter(fractionRadiant, "Fraction Radiant"),
+                Utility.IDFLineFormatter("", "Sensible Heat Fraction"),
+                Utility.IDFLineFormatter("People Activity Schedule", "Activity Level Schedule Name"),
+                Utility.IDFLineFormatter(c02genRate, "Carbon Dioxide Generation Rate {m3/s-W}"),
+                Utility.IDFLineFormatter("", "Enable ASHRAE 55 Comfort Warnings"),
+                Utility.IDFLineFormatter(meanRadiantTempCalcType, "Mean Radiant Temperature Calculation Type"),
+                Utility.IDFLineFormatter("", "Surface Name/Angle Factor List Name"),
+                Utility.IDFLineFormatter("Work Eff Sch", "Work Efficiency Schedule Name"),
+                Utility.IDFLineFormatter(clothingInsulationCalcMeth, "Clothing Insulation Calculation Method"),
+                Utility.IDFLineFormatter("", "Clothing Insulation Calculation Method Schedule Name"),
+                Utility.IDFLineFormatter("", "Clothing Insulation Schedule Name"),
+                Utility.IDFLineFormatter("Air Velo Sch", "Air Velocity Schedule Name"),
+                Utility.IDFLastLineFormatter(thermalComfModel1t, "Thermal Comfort Model 1 Type")
+            };
         }
     }
     [Serializable]
